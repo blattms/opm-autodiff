c812ff20340ab2e68436c6c2803414e71ff25baf
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2019-12-06 23:25:44 +0100

    add mpi serialization for DynamicVector

--- opm/simulators/utils/ParallelRestart.cpp
+++ opm/simulators/utils/ParallelRestart.cpp
@@ -171,6 +171,12 @@ std::size_t packSize(const DynamicState<T>& data, Dune::MPIHelper::MPICommunicat
     return packSize(data.data(), comm) + packSize(data.initialRange(), comm);
 }
 
+template<class T>
+std::size_t packSize(const DynamicVector<T>& data, Dune::MPIHelper::MPICommunicator comm)
+{
+    return packSize(data.data(), comm);
+}
+
 std::size_t packSize(const char* str, Dune::MPIHelper::MPICommunicator comm)
 {
 #if HAVE_MPI
@@ -973,6 +979,13 @@ void pack(const DynamicState<T>& data, std::vector<char>& buffer, int& position,
 {
     pack(data.data(), buffer, position, comm);
     pack(data.initialRange(), buffer, position, comm);
+}
+
+template<class T>
+void pack(const DynamicVector<T>& data, std::vector<char>& buffer, int& position,
+          Dune::MPIHelper::MPICommunicator comm)
+{
+    pack(data.data(), buffer, position, comm);
 }
 
 void pack(const char* str, std::vector<char>& buffer, int& position,
@@ -1846,6 +1859,15 @@ void unpack(DynamicState<T>& data, std::vector<char>& buffer, int& position,
     unpack(ddata, buffer, position, comm);
     unpack(initial_range, buffer, position, comm);
     data = DynamicState<T>(ddata, initial_range);
+}
+
+template<class T>
+void unpack(DynamicVector<T>& data, std::vector<char>& buffer, int& position,
+            Dune::MPIHelper::MPICommunicator comm)
+{
+    std::vector<T> ddata;
+    unpack(ddata, buffer, position, comm);
+    data = DynamicVector<T>(ddata);
 }
 
 void unpack(char* str, std::size_t length, std::vector<char>& buffer, int& position,
